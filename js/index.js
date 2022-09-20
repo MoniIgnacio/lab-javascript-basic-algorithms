7272b16df71d044b308c1dacce8e31d089078271
MoniIgnacio <moniignacio02@gmail.com>
2022-09-20 19:46:23 +0400

    Iteration 2 Done

--- js/index.js
+++ js/index.js
@@ -6,6 +6,10 @@ let hacker2 = 'Horacio';
 console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
-
+let longestName = '';
+for(let i=0; i<hacker2.length;i++){
+    longestName = longestName + hacker2[i];
+}
+console.log(`It seems that the navigator has the longest name, it has ${longestName.length} characters.`)
 
 // Iteration 3: Loops
