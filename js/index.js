8891fa8c393c9d9969b6dda5b3d76f6a57254af8
MoniIgnacio <moniignacio02@gmail.com>
2022-09-21 09:45:20 +0400

    Iteration 3 Done

--- js/index.js
+++ js/index.js
@@ -13,3 +13,35 @@ for(let i=0; i<hacker2.length;i++){
 console.log(`It seems that the navigator has the longest name, it has ${longestName.length} characters.`)
 
 // Iteration 3: Loops
+
+let hacker1Capitals = '';
+for(let i=0; i<hacker1.length;i++){
+    hacker1Capitals += hacker1[i] + ' ';
+}
+hacker1Capitals = hacker1Capitals.toUpperCase();
+
+console.log(hacker1Capitals);
+
+let hacker2Reverse = '';
+for(let i=hacker2.length -1; i>=0;i--){
+    hacker2Reverse += hacker2[i]; 
+}
+console.log(hacker2Reverse);
+
+
+
+for(let i=0; i< hacker1.length;i++){
+    if(hacker1[i]<hacker2[i]){
+        console.log("The driver's name goes first");
+        break;
+    }else if (hacker1[i]>hacker2[i]){
+        console.log("Yo, the navigator goes first definitely");
+        break;
+    }else  if (hacker1[i]===hacker2[i]){
+        console.log ("What? you both have the same name" );
+    }else {
+        break;
+    }
+}
+
+
